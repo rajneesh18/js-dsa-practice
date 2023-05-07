13b8e7b4f2aa49f8a6a34a20032958d8f728af8d
rajneesh18 <44999300+rajneesh18@users.noreply.github.com>
2023-05-07 22:40:57 +0530

    2667-HelloWorld

---
+++ leetcode/2667-HelloWorld.js
@@ -0,0 +1,14 @@
+/**
+ * 2667. Create Hello World Function
+ * Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ */
+
+/**
+ * Hello World
+ * @return Function
+ */
+export let createHelloWorld = () => {
+  return (...args) => {
+    return 'Hello World';
+  };
+};
