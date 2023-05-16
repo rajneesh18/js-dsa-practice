273b57eef307c36217d8b7280a163faae42984b5
rajneesh18 <44999300+rajneesh18@users.noreply.github.com>
2023-05-16 16:01:39 +0530

    2632.Curry

--- leetcode/2632.Curry.js
+++ leetcode/2632.Curry.js
@@ -12,6 +12,18 @@
  * @param {Function} fn
  * @return {Function}
  */
-var curry = function (fn) {
-  return function curried() {};
+export var curry = function (fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+
+    return (...nextargs) => curried(...args, ...nextargs);
+  };
 };
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
